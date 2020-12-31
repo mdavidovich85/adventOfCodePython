960a5b9c7cc20bf4d8b613f72b8ed5a1efe38dd2
Davidovich, Mike <Michael.X.Davidovich@HealthPartners.Com>
2020-12-31 11:12:20 -0600

    Day 5, part 1 complete

--- 2015/day5.py
+++ 2015/day5.py
@@ -1,20 +1,43 @@
+# https://adventofcode.com/2015/day/5
 import os
-start = "/aocPython/2015/input/inputDay5.txt"
+start = "/adventOfCodePython/2015/input/inputDay5.txt"
 path=os.path.realpath(start)
 # defaults to read
 vowels = "AaEeIiOoUu"
+pairs=["ab","cd","pq","xy"]
 f = open(path)
 
 def Check_Vow(string, vowels): 
     final = [each for each in string if each in vowels] 
     return final
 
+niceCount=0
 while True:
     line=f.readline()
     if not line:
         break
-    if Check_Vow(line,vowels)>=3:
-        print("keep checking")
-
+    # check if 3 or more vowels
+    if len(Check_Vow(line,vowels))>=3:
+        #check if there's a double letter e.g. 'dd'
+        prevLetter=line[0]
+        doubleMatch=False
+        for letter in range(len(line)-1):
+            thisLetter=line[letter+1]
+            if prevLetter==thisLetter:
+                doubleMatch=True
+            prevLetter=thisLetter
+            #check for letter pairs
+        prevLetter=line[0]
+        pairMatch=False        
+        if doubleMatch==True:
+            for letter in range(len(line)-1):
+                thisLetter=line[letter+1]
+                if prevLetter+thisLetter in pairs:
+                    pairMatch=True
+                prevLetter=thisLetter
+            if pairMatch!=True:
+                niceCount+=1        
 
 f.close()
+
+print("Part 1 Answer: "+ str(niceCount))
